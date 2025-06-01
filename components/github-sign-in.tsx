33de05d04897c13ccd902e4fca9faa4d23fad40a
bahori <bahori2011ms@gmail.com>
2025-06-01 22:18:51 +0900

    reset 2

---
+++ components/github-sign-in.tsx
@@ -0,0 +1,17 @@
+import { signIn } from "@/lib/auth";
+import { Button } from "./ui/button";
+import { Github } from "./ui/github";
+
+export function GithubSignIn() {
+  return (
+    <form action={async () => {
+      "use server";
+      await signIn("github")
+    }}>
+      <Button className="w-full" variant="outline">
+        <Github />
+        Continue with GitHub
+      </Button>
+    </form>
+  )
+}
---
+++ lib/env.ts
@@ -0,0 +1,18 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z.string().url(),
+    GITHUB_CLIENT_ID: z.string(),
+    GITHUB_CLIENT_SECRET: z.string(),
+    AUTH_SECRET: z.string(),
+  },
+  client: {},
+  experimental__runtimeEnv: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+  },
+});
